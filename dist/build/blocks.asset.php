749e758d771da48f09099695f0081bc9b8e02be6
RajkiranBagal <rajkiranb@bsf.io>
2021-04-23 14:45:04 +0530

    Phpcs

--- dist/build/blocks.asset.php
+++ dist/build/blocks.asset.php
@@ -1,1 +1,11 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'dcbeba80076aa1e0d242e346010260a9');
+<?php
+/**
+ * Block Assets
+ *
+ * @package UAGB
+ */
+
+return array(
+	'dependencies' => array( 'react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill' ),
+	'version'      => 'dcbeba80076aa1e0d242e346010260a9',
+);
