7020ea34bbbff53abee43a32b3b3b8fc1688e898
RajkiranBagal <rajkiranb@bsf.io>
2021-04-23 15:06:16 +0530

    Added CSS for Backward compatibility

--- dist/build/blocks.asset.php
+++ dist/build/blocks.asset.php
@@ -1,11 +1,1 @@
-<?php
-/**
- * Block Assets
- *
- * @package UAGB
- */
-
-return array(
-	'dependencies' => array( 'react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill' ),
-	'version'      => 'dcbeba80076aa1e0d242e346010260a9',
-);
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'dcbeba80076aa1e0d242e346010260a9');
