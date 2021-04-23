8d4af42f08b1088666c150a4c6cabe110a0ff905
RajkiranBagal <rajkiranb@bsf.io>
2021-04-23 16:38:59 +0530

    phpcs

--- dist/build/blocks.asset.php
+++ dist/build/blocks.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '00ea76ab35eddc7c8007a0c5b9358df6');
+<?php return array(
+	'dependencies' => array( 'react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill' ),
+	'version'      => '00ea76ab35eddc7c8007a0c5b9358df6',
+);
