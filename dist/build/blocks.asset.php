11a99b33410b207ce5dc187266d12df679ce8a3e
RajkiranBagal <rajkiranb@bsf.io>
2021-04-23 16:54:49 +0530

    Reduced some css

--- dist/build/blocks.asset.php
+++ dist/build/blocks.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill' ),
-	'version'      => '00ea76ab35eddc7c8007a0c5b9358df6',
-);
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '590de1dab60751dce0f164afedb204a0');
