2474cda354ec9e2cc86f03ab0d9dfa4a4a524d0b
Rohit Patil <rohitp@bsf.io>
2021-04-27 11:21:03 +0530

    fix: Converted the class based to functional component

--- dist/build/blocks.asset.php
+++ dist/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9668ec14c66e7bd1dea2cf70c236cf7d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8ff9fbc572cbd265612bbc5d319b00cf');
